bf1c20d35a5d861895b5e9897d9f602a78fe1839
ressKim <sh21dlat@gmail.com>
2021-08-08 20:11:20 +0900

    관심사 분리해 보기

--- spring_study/spring-main/src/main/java/study/core/OrderApp.java
+++ spring_study/spring-main/src/main/java/study/core/OrderApp.java
@@ -11,8 +11,11 @@ import study.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- spring_study/spring-main/src/test/java/study/core/member/MemberServiceTest.java
+++ spring_study/spring-main/src/test/java/study/core/member/MemberServiceTest.java
@@ -1,12 +1,20 @@
 package study.core.member;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import study.core.AppConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
