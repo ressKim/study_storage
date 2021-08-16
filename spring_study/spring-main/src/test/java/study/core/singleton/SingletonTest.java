4e154a1ebe5637bbf3ec222f909764130fc53a56
ressKim <sh21dlat@gmail.com>
2021-08-17 03:30:07 +0900

    singleton pattern study

--- spring_study/spring-main/src/test/java/study/core/singleton/SingletonTest.java
+++ spring_study/spring-main/src/test/java/study/core/singleton/SingletonTest.java
@@ -6,13 +6,15 @@ import org.junit.jupiter.api.Test;
 import study.core.AppConfig;
 import study.core.member.MemberService;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     /**
      * 싱글톤이 적용되지 않은 객체는 new 를 할 때마다 새로운 객체를 생성한다.
      * 그러면 100만명의 사용자가 접근을 했을 때에는 100만 개의 객체가 새로 생성이 된다.
      * 이러면 메모리가 만들어졌다 지워졌다 낭비가 매우 심하게 된다.
-     *
+     * <p>
      * 이런 것을 보안하기 위해 싱글톤 을 사용해야 한다.
      */
 
@@ -32,8 +34,23 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         //memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
 
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        /**
+         * isSameAs ==
+         * isEqualAs equals
+         */
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
 
