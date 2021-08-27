e0ca592c7deaf13501d0a9cebadbf2ffe8e81df1
ressKim <sh21dlat@gmail.com>
2021-08-27 09:23:25 +0900

    lombok 추가 및 RequiredArgsConstructor 적용해보기

--- spring_study/spring-main/src/main/java/study/core/order/OrderServiceImpl.java
+++ spring_study/spring-main/src/main/java/study/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package study.core.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import study.core.discount.DiscountPolicy;
@@ -7,22 +8,21 @@ import study.core.member.Member;
 import study.core.member.MemberRepository;
 
 /**
- * 생성자 주입을 생각해 보자. - 이걸 쓰는게 좋다.
- * 생성자 주입을 선택하고, 수정자 주입이 필요한 경우가 있다면 그때 선택해서 사용하자.
+ * 생성자 주입을 생각해 보자. - 이걸 쓰는게 좋다. 생성자 주입을 선택하고, 수정자 주입이 필요한 경우가 있다면 그때 선택해서 사용하자.
  */
-@Component
-public class OrderServiceImpl implements OrderService {
+//@Component
+//public class OrderServiceImpl implements OrderService {
 
-  /**
-   * @Autowired -- 의존성 주입 방법중 얼마전까지 많이 사용되던 필드 주입 방식이다 - 이 방식은 테스트 코드를 짠다고 가정해서 생각하면 관련된 test 코드 전체가
-   * 스프링에 의존하는 경우가 발생하게 된다. 어플리케이션과 관계없는 테스트도 말이다. 그래서 이거의 사용은 왠만하면 피하자.
-   */
+/**
+ * @Autowired -- 의존성 주입 방법중 얼마전까지 많이 사용되던 필드 주입 방식이다 - 이 방식은 테스트 코드를 짠다고 가정해서 생각하면 관련된 test 코드 전체가
+ * 스프링에 의존하는 경우가 발생하게 된다. 어플리케이션과 관계없는 테스트도 말이다. 그래서 이거의 사용은 왠만하면 피하자.
+ */
 //    @Autowired private MemberRepository memberRepository;
 //    @Autowired private DiscountPolicy discountPolicy;
 
-  //의존관계를 주입하는 방법에는 다양한 방법들이 있다.
-  //이렇게 넣는거를 수정자 주입이라고 한다 . (사실 알아만 두자)
-  //밑에 setXX 를 나눠서 할 수 있기때문에 선택적으로 주입을 할 수도 있다.
+//의존관계를 주입하는 방법에는 다양한 방법들이 있다.
+//이렇게 넣는거를 수정자 주입이라고 한다 . (사실 알아만 두자)
+//밑에 setXX 를 나눠서 할 수 있기때문에 선택적으로 주입을 할 수도 있다.
     /*
     @Autowired
     public void setMemberRepository(MemberRepository memberRepository){
@@ -46,14 +46,12 @@ public class OrderServiceImpl implements OrderService {
 
      */
 
-  private MemberRepository memberRepository;
-  private DiscountPolicy discountPolicy;
+@Component
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
 
-  @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
+  private final MemberRepository memberRepository;
+  private final DiscountPolicy discountPolicy;
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
