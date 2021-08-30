45081cba489b5cea74e16e301dbd1250e4cfce98
ressKim <sh21dlat@gmail.com>
2021-08-30 10:19:43 +0900

    bean 등록 초기화, 소멸 method study

--- spring_study/spring-main/src/test/java/study/core/lifecycle/BeanLifeCycleTest.java
+++ spring_study/spring-main/src/test/java/study/core/lifecycle/BeanLifeCycleTest.java
@@ -37,7 +37,26 @@ class BeanLifeCycleTest {
 
   @Configuration
   static class LifeCycleConfig{
-    @Bean
+
+    /**
+     * 설정 정보 사용의 특징
+     * 메서드 이름을 자유롭게 줄 수 있다.
+     * 스프링 빈이 스프링 코드에 의존하지 않는다.
+     * 코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.
+     */
+
+    /**
+     * 추가로 종료메서드 추론 이라는게 있다.
+     * @Bean 의 destroyMethod 에는 특별한 기능이 있다.
+     * 라이브러리는 대부분 'close', 'shutdown' 이라는 종료 메서드를 사용을 한다.
+     * @Bean 의 'destoryMethod' 는 기본값이 '(inferred)'(추론) 으로 등록되어 있다.
+     * 이 추론 기능은 'close', 'shutdown'라는 이름의 메서드를 자동으로 호출 해 준다.
+     * 이름 그대로 종료 메서드를 추론해서 호출 해 준다.
+     * 따라서 직접 스프링 빈으로 등록하면 종료 메서드는 적어주지 않아도 되는경우가 발생한다.
+     * 추론 기능을 사용하기 싫다면 'destroyMethod=""' 처럼 빈 공백을 사용하면 된다.
+     *ㅏ
+     */
+    @Bean(initMethod = "init", destroyMethod = "close")
     public NetworkClient networkClient(){
       NetworkClient networkClient = new NetworkClient();
       networkClient.setUrl("http://hello-spring.dd");
--- spring_study/spring-main/src/test/java/study/core/lifecycle/NetworkClient.java
+++ spring_study/spring-main/src/test/java/study/core/lifecycle/NetworkClient.java
@@ -3,7 +3,8 @@ package study.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+//public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
   private String url;
 
@@ -33,7 +34,27 @@ public class NetworkClient implements InitializingBean, DisposableBean {
   public void disconnect() {
     System.out.println("disconnect = " + url);
   }
+
+  public void init(){
+    System.out.println("NetworkClient.init");
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+  public void close(){
+    System.out.println("NetworkClient.close");
+    disconnect();
+  }
+
+
+
+
+
+
+
+
   /*
+  implements InitializingBean, DisposableBean 사용 시
   초기화, 소멸 인터페이스의 단점
   - 이 인터페이스는 스프링 전용 인터페이스이기 때문에, 스프링 전용 인터페이스에 의존하게 된다.
   - 초기화, 소멸 메소드의 이름을 변경할 수 없다.
@@ -42,16 +63,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
   -->> 인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 더 나은 방법들이 있어서 거의 사용하지는 않는다.
    */
 
-  @Override
-  public void afterPropertiesSet() throws Exception {
-    System.out.println("NetworkClient.afterPropertiesSet");
-    connect();
-    call("초기화 연결 메시지");
-  }
-
-  @Override
-  public void destroy() throws Exception {
-    System.out.println("NetworkClient.destroy");
-    disconnect();
-  }
+//  @Override
+//  public void afterPropertiesSet() throws Exception {
+//    System.out.println("NetworkClient.afterPropertiesSet");
+//    connect();
+//    call("초기화 연결 메시지");
+//  }
+//
+//  @Override
+//  public void destroy() throws Exception {
+//    System.out.println("NetworkClient.destroy");
+//    disconnect();
+//  }
 }
