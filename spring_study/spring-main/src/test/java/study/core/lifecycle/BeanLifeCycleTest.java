b4ec8dd7cf07c740b711d62f8ea0dddeaf7d5b39
ressKim <sh21dlat@gmail.com>
2021-08-30 09:55:08 +0900

    bean lifecycle study 02

--- spring_study/spring-main/src/test/java/study/core/lifecycle/BeanLifeCycleTest.java
+++ spring_study/spring-main/src/test/java/study/core/lifecycle/BeanLifeCycleTest.java
@@ -8,6 +8,21 @@ import org.springframework.context.annotation.Configuration;
 
 public class BeanLifeCycleTest {
 
+  /**
+   * 스프링 빈의 이벤트 라이프 사이클 - (싱글톤에 관해서)
+   * 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
+   *
+   * - 초기화 콜백 : 빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출
+   * - 소멸전 콜백 : 빈이 소멸되기 직전에 호출
+   *
+   * ++ 참고 : 객체의 생성과 초기화를 분리하자.
+   * >> 생성자는 필수정보(파라미터) 를 받고, 메모리를 할당해서 객체를 생성하는 책임을 가진다. 반면에 초기화는 이렇게 생성된 값들을 활용해
+   * 외부 커넥션을 연결하는 등 무거운 동작을 수행한다.
+   *
+   * >> 따라서 생성자 안에서 무거운 초기화 작업을 함께 하는 것 보다는 객체를 생성하는 부분과 초기화 하는 부분을 명확하게 나누는것이 유지보수 관점에서 좋다.
+   * 물론 초기화 작업이 내부 값들만 약간 변경하는 정도라면은 생성자에서 한번에 처리하는게 더 나을 수 있다.
+   */
+
   @Test
   void lifeCycleTest(){
     ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(
--- spring_study/spring-main/src/test/java/study/core/lifecycle/NetworkClient.java
+++ spring_study/spring-main/src/test/java/study/core/lifecycle/NetworkClient.java
@@ -4,7 +4,7 @@ public class NetworkClient {
 
   private String url;
 
-  public NetworkClient(){
+  public NetworkClient() {
     System.out.println("url = " + url);
     connect();
     call("초기화 연결 메시지");
@@ -15,18 +15,19 @@ public class NetworkClient {
   public void setUrl(String url) {
     this.url = url;
   }
+
   //서비스 시작시 호출
-  public void connect(){
+  public void connect() {
     System.out.println("connect : " + url);
   }
 
-  public void call(String message){
+  public void call(String message) {
     System.out.println("call = " + url + "message = " + message);
 
   }
 
   //서비스 종료시 호출
-  public void disconnect(){
+  public void disconnect() {
     System.out.println("disconnect = " + url);
   }
 }
