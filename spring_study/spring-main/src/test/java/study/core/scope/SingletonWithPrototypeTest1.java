d297cfc694a6eced118d4be6541f561d77d24c56
hyukjunKim <sh21dlat@gmail.com>
2021-09-01 14:33:14 +0900

    Javax의 Provider study

--- spring_study/spring-main/src/test/java/study/core/scope/SingletonWithPrototypeTest1.java
+++ spring_study/spring-main/src/test/java/study/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
@@ -44,11 +45,35 @@ class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean {
 
+        //ObjectProvider 같은경우 spring 에서 제공한다 . 즉 spring 에 의존적이다.
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        /*
+        Provider 는 javax에서 제공하는 것이다 (gradle에 추가해줘야된다.)
+        장점은 간단하다 , 단점도 간단하다
+         */
+        /**
+         * 프로토타입 빈은 언제 사용할까??
+         * 매번 사용할 때마다 의존관계 주입이 완료된것이 필요하다면 쓴다.
+         * 그렇지만 싱글톤 내에서 거의 해결이 되서 거ㅓㅓㅓ의 안쓴다고 보면 된다.
+         * javax의 Provider 에서는 이렇게 설명한다.
+         * retrieving multiple instances.
+         * lazy or optional retrieval of an instance.
+         * breaking circular dependencies.
+         * abstracting scope so you can look up an instance in a smaller scope from an instance in a containing scope.
+         *
+         * -(거의 사용 안한다고 생각해도 된다
+         */
+        /**
+         * 그럼 어떤걸 쓸까?? spring 에서 제공하는 ObjectProvider
+         * java 표준 javax 의 Provider?
+         * jpa는 java표준이 이겼지만 spring 과의 비교에서는 spring꺼를 쓰는게 현재로써는
+         * 편하다.
+         */
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
