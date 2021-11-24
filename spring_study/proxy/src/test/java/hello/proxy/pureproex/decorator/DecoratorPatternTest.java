0dcf549863ecd89e56510f323c2b78ddf4417a4f
ressKim <sh21dlat@gmail.com>
2021-11-24 23:47:52 +0900

    데코레이터 패턴 적용 2

--- spring_study/proxy/src/test/java/hello/proxy/pureproex/decorator/DecoratorPatternTest.java
+++ spring_study/proxy/src/test/java/hello/proxy/pureproex/decorator/DecoratorPatternTest.java
@@ -2,9 +2,11 @@ package hello.proxy.pureproex.decorator;
 
 import hello.proxy.pureproex.decorator.code.Component;
 import hello.proxy.pureproex.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproex.decorator.code.MessageDecorator;
 import hello.proxy.pureproex.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.support.MessageSourceAccessor;
 
 @Slf4j
 public class DecoratorPatternTest {
@@ -14,6 +16,15 @@ public class DecoratorPatternTest {
     Component realComponent = new RealComponent();
     DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
     client.execute();
+  }
+
+  @Test
+  void decorator1(){
+    Component realComponent = new RealComponent();
+    Component messageComponent = new MessageDecorator(realComponent);
+    DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
+    client.execute();
+
   }
 
 }
