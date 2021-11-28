2c27f14d26954591fa503db660cf6f3af0063a0d
ressKim <sh21dlat@gmail.com>
2021-11-28 23:05:57 +0900

    인터페이스 기반 프록시 추가 및 적용

--- spring_study/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_study/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,19 +2,29 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //이렇게 해서 범위 밖의 설정파일들을 추가할려고 한다.
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 //이렇게 지정을 했는데 이거는 나중에 설정파일만을 변경하기 위해서 하위의 app 만 componentScan 을 하게 둔다
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
--- spring_study/proxy/src/test/java/hello/proxy/pureproex/decorator/DecoratorPatternTest.java
+++ spring_study/proxy/src/test/java/hello/proxy/pureproex/decorator/DecoratorPatternTest.java
@@ -4,6 +4,7 @@ import hello.proxy.pureproex.decorator.code.Component;
 import hello.proxy.pureproex.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproex.decorator.code.MessageDecorator;
 import hello.proxy.pureproex.decorator.code.RealComponent;
+import hello.proxy.pureproex.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.MessageSourceAccessor;
@@ -12,17 +13,27 @@ import org.springframework.context.support.MessageSourceAccessor;
 public class DecoratorPatternTest {
 
   @Test
-  void noDecorator(){
+  void noDecorator() {
     Component realComponent = new RealComponent();
     DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
     client.execute();
   }
 
   @Test
-  void decorator1(){
+  void decorator1() {
     Component realComponent = new RealComponent();
     Component messageComponent = new MessageDecorator(realComponent);
     DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
+    client.execute();
+
+  }
+
+  @Test
+  void decorator2() {
+    Component realComponent = new RealComponent();
+    Component messageDecorator = new MessageDecorator(realComponent);
+    TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+    DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
     client.execute();
 
   }
