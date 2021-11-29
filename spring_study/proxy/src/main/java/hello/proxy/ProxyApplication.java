33f1ef7440045538d4a8ef1300f26e313f618c6c
ressKim <sh21dlat@gmail.com>
2021-11-29 21:40:18 +0900

    구체 클래스 기반 프록시 예제 3(concrete version)

--- spring_study/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_study/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Import;
 //이렇게 해서 범위 밖의 설정파일들을 추가할려고 한다.
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 //이렇게 지정을 했는데 이거는 나중에 설정파일만을 변경하기 위해서 하위의 app 만 componentScan 을 하게 둔다
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
