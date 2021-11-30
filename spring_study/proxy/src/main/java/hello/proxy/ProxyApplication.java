dd6426aeede5df70890355243060db0f1940fb76
ressKim <sh21dlat@gmail.com>
2021-11-30 23:23:56 +0900

    jdk 의 동적프록시 코드에 적용 -nolog 처리

--- spring_study/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_study/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -18,7 +19,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 //이렇게 지정을 했는데 이거는 나중에 설정파일만을 변경하기 위해서 하위의 app 만 componentScan 을 하게 둔다
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
