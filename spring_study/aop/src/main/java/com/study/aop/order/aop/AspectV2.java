402a4202fa28b969aaa2637e03f4334a6b7d50ec
ressKim <sh21dlat@gmail.com>
2021-12-10 22:25:19 +0900

    spring aop 구현 연습 2 - pointcut 분리

--- spring_study/aop/src/main/java/com/study/aop/order/aop/AspectV2.java
+++ spring_study/aop/src/main/java/com/study/aop/order/aop/AspectV2.java
@@ -4,13 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
+public class AspectV2 {
 
+  //이렇게 나누어서 사용할 수도 있다.
+  
+  //만약에 포인트컷을 모아서 사용할 경우 private 말고 상황에 맞게 쓰면 된다.
   //com.study.aop.order 패키지와 하위 패키지
-  @Around("execution(* com.study.aop.order..*(..))")
+  @Pointcut("execution(* com.study.aop.order..*(..))")
+  private void allOrder(){} //pointcut signature
+
+  @Around("allOrder()")
   public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{
     //join point 시그니처
     log.info("[log] {}", joinPoint.getSignature());
