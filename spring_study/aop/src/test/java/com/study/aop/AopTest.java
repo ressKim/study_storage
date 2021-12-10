c1d99f1693b6f764fa809d571ada37f41344234d
ressKim <sh21dlat@gmail.com>
2021-12-10 22:50:38 +0900

    spring aop 구현 연습 4 - 공용 포인트컷으로 빼서 적용

--- spring_study/aop/src/test/java/com/study/aop/AopTest.java
+++ spring_study/aop/src/test/java/com/study/aop/AopTest.java
@@ -2,15 +2,13 @@ package com.study.aop;
 
 import com.study.aop.order.OrderRepository;
 import com.study.aop.order.OrderService;
-import com.study.aop.order.aop.AspectV1;
-import com.study.aop.order.aop.AspectV2;
 import com.study.aop.order.aop.AspectV3;
+import com.study.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
@@ -18,7 +16,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
 
   @Autowired
