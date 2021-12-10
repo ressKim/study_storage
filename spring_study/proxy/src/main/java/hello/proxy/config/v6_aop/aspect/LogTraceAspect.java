a2c54f8704b6a9f21fc0ee35be3819a5d199dec0
ressKim <sh21dlat@gmail.com>
2021-12-10 20:44:36 +0900

    Aspect 프록시 study 내용정리

--- spring_study/proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
+++ spring_study/proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -2,7 +2,6 @@ package hello.proxy.config.v6_aop.aspect;
 
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
-import java.lang.reflect.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -18,8 +17,18 @@ public class LogTraceAspect {
     this.logTrace = logTrace;
   }
 
+  /**
+   * 어드바이져 = 포인트컷 + 어드바이스 이다.
+   * 자동 프록시 생성기 (' AnnotationAwareAspectJAutoProxyCreator ')은
+   * @Aspect 를 찾아서 이것을 'Advisor' 로 만들어 주는 기능도 있다.
+   * 그래서 두가지 동작을 한다.
+   * 1. @Aspect 를 확인하고 Advisor 로 변환해서 저장.
+   * 2. 어드바이저를 기반으로 프록시를 생성.
+   */
   @Around("execution(* hello.proxy.app..*(..))")
   public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+    //Advice 로직
 
     TraceStatus status = null;
     try {
