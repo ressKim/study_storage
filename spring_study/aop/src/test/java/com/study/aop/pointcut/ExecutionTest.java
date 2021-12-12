75f0b93116a289935e79087c9b6b0d188a1208cf
ressKim <sh21dlat@gmail.com>
2021-12-12 22:09:54 +0900

    spring aop pointcut 문법 study 를 위한 예제 생성

---
+++ spring_study/aop/src/test/java/com/study/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,28 @@
+package com.study.aop.pointcut;
+
+import com.study.aop.member.MemberServiceImpl;
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+@Slf4j
+public class ExecutionTest {
+
+  AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+  Method helloMethod;
+
+  @BeforeEach
+  public void init() throws NoSuchMethodException {
+    helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+  }
+
+  @Test
+  void printMethod(){
+    //execution( * ..package..Class.)
+    //public java.lang.String com.study.aop.member.MemberServiceImpl.hello(java.lang.String)
+    log.info("helloMethod= {}", helloMethod);
+  }
+
+}
