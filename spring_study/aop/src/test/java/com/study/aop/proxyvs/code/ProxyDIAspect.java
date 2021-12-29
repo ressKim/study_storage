7dea3d19e06fbb488baceb6dd42b9359938dc07e
ressKim <sh21dlat@gmail.com>
2021-12-29 22:17:58 +0900

    프록시 기술과 한계 - 의존관계 주입

---
+++ spring_study/aop/src/test/java/com/study/aop/proxyvs/code/ProxyDIAspect.java
@@ -0,0 +1,17 @@
+package com.study.aop.proxyvs.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class ProxyDIAspect {
+
+  @Before("execution(* com.study.aop..*.*(..))")
+  public void doTrace(JoinPoint joinpoint) {
+    log.info("[proxyDIAdvice {}", joinpoint.getSignature());
+  }
+
+}
