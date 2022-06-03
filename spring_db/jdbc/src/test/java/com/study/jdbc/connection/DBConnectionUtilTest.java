79070b73265d7d8c0b187178fcb163330be81aa3
ressKim <sh21dlat@gmail.com>
2022-06-03 23:29:42 +0900

    DB 연결 및 동작 방식 study

---
+++ spring_db/jdbc/src/test/java/com/study/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,18 @@
+package com.study.jdbc.connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+  @Test
+  void connection(){
+    Connection connection = DBConnectionUtil.getConnection();
+    assertThat(connection).isNotNull();
+  }
+
+}
