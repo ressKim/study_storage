39de09a1239ddc5a053542df9ecc6d3c0427584f
ressKim <sh21dlat@gmail.com>
2022-06-09 22:50:19 +0900

    jdbc 개발 - update, delete

--- spring_db/jdbc/src/test/java/com/study/jdbc/repository/MemberRepositoryV0Test.java
+++ spring_db/jdbc/src/test/java/com/study/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,12 @@
 package com.study.jdbc.repository;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.study.jdbc.domain.Member;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -18,7 +18,7 @@ class MemberRepositoryV0Test {
   void crud() throws SQLException {
 
     //save
-    Member member = new Member("memberV3", 10000);
+    Member member = new Member("memberV100", 10000);
     repository.save(member);
 
     //findById
@@ -27,5 +27,17 @@ class MemberRepositoryV0Test {
     log.info("member == findMember {}", member == findMember);
 
     assertThat(findMember).isEqualTo(member);
+
+    //update: money : 10000 -> 20000
+    repository.update(member.getMemberId(), 20000);
+    Member updatedMember = repository.findById(member.getMemberId());
+
+    assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+    //delete
+    repository.delete(member.getMemberId());
+    assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+        .isInstanceOf(NoSuchElementException.class);
+
   }
 }
