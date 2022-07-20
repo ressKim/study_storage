6616662d6ac7fbb4a5870512ed2e0511a4dd2753
ressKim <sh21dlat@gmail.com>
2022-07-20 13:38:22 +0900

    트랜잭션 템플릿을 사용

---
+++ spring_db/jdbc/src/main/java/com/study/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,68 @@
+package com.study.jdbc.service;
+
+import com.study.jdbc.domain.Member;
+import com.study.jdbc.repository.MemberRepositoryV3;
+import java.sql.Connection;
+import java.sql.SQLException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+//  private final PlatformTransactionManager transactionManager;
+  private final TransactionTemplate txTemplate;
+  private final MemberRepositoryV3 memberRepository;
+
+  public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+    this.txTemplate = new TransactionTemplate(transactionManager);
+    this.memberRepository = memberRepository;
+  }
+
+  public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+    txTemplate.executeWithoutResult((status) -> {
+      try {
+        bizLogic(fromId, toId, money);
+      } catch (SQLException e) {
+        throw new IllegalStateException(e);
+      }
+    });
+  }
+
+  private void bizLogic(String fromId, String toId, int money)
+      throws SQLException {
+    Member fromMember = memberRepository.findById(fromId);
+    Member toMember = memberRepository.findById(toId);
+
+    memberRepository.update(fromId, fromMember.getMoney() - money);
+    validation(toMember);
+    memberRepository.update(toId, toMember.getMoney() + money);
+  }
+
+  private void release(Connection con) {
+    if (con != null) {
+      try {
+        con.setAutoCommit(true);//보통은 true 이기 때문에 돌려줘야 한다.
+        con.close();
+      } catch (Exception e) {
+        //exception 을 로그로 남기면 {} 안쓰고 이렇게 써도 된다
+        log.info("error", e);
+      }
+    }
+  }
+
+  private void validation(Member toMember) {
+    if (toMember.getMemberId().equals("ex")) {
+      throw new IllegalStateException("이체중 예외 발생");
+    }
+  }
+
+}
