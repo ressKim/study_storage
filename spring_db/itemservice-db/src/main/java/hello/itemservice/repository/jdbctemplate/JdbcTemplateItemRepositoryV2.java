d2301c4571f5dc21b055121dca8d4962238b8f3b
ressKim <sh21dlat@gmail.com>
2022-08-09 20:36:05 +0900

    jdbcTemplate - 이름 지정 파라미터 대입

--- spring_db/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ spring_db/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -61,7 +61,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
   public void update(Long itemId, ItemUpdateDto updateParam) {
     String sql = "update item "
         + "set item_name=:itemName, price=:price, quantity=:quantity "
-        + "where id = ?";
+        + "where id = :id";
 
     SqlParameterSource param = new MapSqlParameterSource()
         .addValue("itemName", updateParam.getItemName())
