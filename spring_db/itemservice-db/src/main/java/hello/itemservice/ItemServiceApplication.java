b747c6be773b84f616c92634875112468de671ea
ressKim <sh21dlat@gmail.com>
2022-08-25 18:33:28 +0900

    Querydsl 추가 및 적용해 보기

--- spring_db/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring_db/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -19,7 +19,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MybatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
