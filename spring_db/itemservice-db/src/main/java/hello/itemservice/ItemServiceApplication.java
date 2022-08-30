4d04f7d5a9515d4358bca8b39a3a893e0cf4b057
ressKim <sh21dlat@gmail.com>
2022-08-30 20:04:21 +0900

    spring data jpa 와 querydsl 로 실용적인 구조 적용

--- spring_db/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring_db/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -20,7 +20,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 //@Import(MybatisConfig.class)
 //@Import(JpaConfig.class)
 //@Import(SpringDataJpaConfig.class)
-@Import(QuerydslConfig.class)
+//@Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
