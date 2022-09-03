ea5f610f444aa9a841a23fd2f4e5e1550d6de893
ressKim <sh21dlat@gmail.com>
2022-09-03 21:16:08 +0900

    예외와 트랜잭션 커밋-롤백 활용 2

--- spring_db/springtx/src/test/java/hello/springtx/exception/RollbackTest.java
+++ spring_db/springtx/src/test/java/hello/springtx/exception/RollbackTest.java
@@ -3,7 +3,6 @@ package hello.springtx.exception;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
